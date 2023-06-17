679bd630b5761c06d4e8e691a727e7e079e90828
Maneesha Walisundera <moonlit741@gmail.com>
2023-06-17 16:30:29 +0530

    navbar sroll issue fixed

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // change nav style
+const body = document.body;
+let lastScroll = 0;
+
 window.addEventListener("scroll", () => {
   document
     .querySelector("nav")
-    .classList.toggle("window-scrolled", window.screenY > 0);
+    .classList.toggle("window-scrolled", window.scrollY > 0);
 });
