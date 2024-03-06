56eee97e6eddfecd1f42a197461cf0650c441888
Maneesha <maneesha14w@gmail.com>
2024-03-06 13:10:21 +0530

    feat: done with gallery and removed old folder.

--- old/main.js
+++
@@ -1,9 +0,0 @@
-// change nav style
-const body = document.body;
-let lastScroll = 0;
-
-window.addEventListener("scroll", () => {
-  document
-    .querySelector("nav")
-    .classList.toggle("window-scrolled", window.scrollY > 0);
-});
--- wes-portfolio/src/components/Gallery/Gallery.jsx
+++ wes-portfolio/src/components/Gallery/Gallery.jsx
@@ -9,27 +9,26 @@ const Gallery = () => {
           <img
             className="image-grid-col-2"
             src="src/assets/images/model-potrait-1-min.jpg"
-            alt
           />
-          <img src="src/assets/images/model-portrait-6-min.jpg" alt />
-          <img src="src/assets/images/model-portrait-4-min.jpg" alt />
-          <img src="src/assets/images/model-portrait-8-min.jpg" alt />
-          <img src="src/assets/images/aerial-shot-of-town.jpg" alt />
-          <img src="src/assets/images/man-on-boat.jpg" alt />
-          <img src="src/assets/images/golden-landscape.jpg" alt />
-          <img src="src/assets/images/pathway.jpg" alt />
-          <img src="src/assets/images/rice-field.jpg" alt />
-          <img src="src/assets/images/river.jpg" alt />
-          <img src="src/assets/images/horse-min.jpg" alt />
-          <img src="src/assets/images/landscape-with-man-min.jpg" alt />
-          <img src="src/assets/images/man-in-front-of-mountain-min.jpg" alt />
-          <img src="src/assets/images/man-looking-at-water-min.jpg" alt />
-          <img src="src/assets/images/photographer-min.jpg" alt />
-          <img src="src/assets/images/couple-facing-each-other-min.jpg" alt />
-          <img src="src/assets/images/couple-on-grass-min.jpg" alt />
-          <img src="src/assets/images/couple-standing-min.jpg" alt />
-          <img src="src/assets/images/portrait-of-guy-min.jpg" alt />
-          <img src="src/assets/images/potrait-of-couple-min.jpg" alt />
+          <img src="src/assets/images/model-portrait-6-min.jpg" />
+          <img src="src/assets/images/model-portrait-4-min.jpg" />
+          <img src="src/assets/images/model-portrait-8-min.jpg" />
+          <img src="src/assets/images/aerial-shot-of-town.jpg" />
+          <img src="src/assets/images/man-on-boat.jpg" />
+          <img src="src/assets/images/golden-landscape.jpg" />
+          <img src="src/assets/images/pathway.jpg" />
+          <img src="src/assets/images/rice-field.jpg" />
+          <img src="src/assets/images/river.jpg" />
+          <img src="src/assets/images/horse-min.jpg" />
+          <img src="src/assets/images/landscape-with-man-min.jpg" />
+          <img src="src/assets/images/man-in-front-of-mountain-min.jpg" />
+          <img src="src/assets/images/man-looking-at-water-min.jpg" />
+          <img src="src/assets/images/photographer-min.jpg" />
+          <img src="src/assets/images/couple-facing-each-other-min.jpg" />
+          <img src="src/assets/images/couple-on-grass-min.jpg" />
+          <img src="src/assets/images/couple-standing-min.jpg" />
+          <img src="src/assets/images/portrait-of-guy-min.jpg" />
+          <img src="src/assets/images/potrait-of-couple-min.jpg" />
         </div>
       </section>
       ;
