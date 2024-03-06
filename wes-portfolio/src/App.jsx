bfd0c2d3414f2097d4ddceaa5cf1e7601c80a0c4
Maneesha <maneesha14w@gmail.com>
2024-03-06 12:00:10 +0530

    feat: navbar converted.

---
+++ wes-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
---
+++ wes-portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <ul>
+          <li>
+            <a href="index.html">
+              <img
+                className="logo"
+                src="src/assets/logos/logo.png"
+                alt="logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#gallery"> Gallery </a>
+          </li>
+          {/* <li><a href="#contact"> Contact </a></li> */}
+          <ul className="nav-socials">
+            <li>
+              <a href="https://instagram.com/yaknowme.wes" target="_blank">
+                <i className="fa-brands fa-instagram" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/anh.tienn" target="_blank">
+                <i className="fa-brands fa-facebook-f" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:anhtien.visuals@gmail.com"
+                className="connect-btn"
+              >
+                <i className="fa-regular fa-envelope" />
+              </a>
+            </li>
+          </ul>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
