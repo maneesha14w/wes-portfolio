886f1adc7bc9a776a0db5a46acf11895f1c19a1b
Maneesha <maneesha14w@gmail.com>
2024-03-06 13:02:22 +0530

    feat: gallery component complete.

--- wes-portfolio/src/components/Gallery/Gallery.jsx
+++ wes-portfolio/src/components/Gallery/Gallery.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import "./gallery.css";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div>
+      <section id="gallery">
+        <div className="gallery-grid">
+          <h1 className="gallery-text">Gallery</h1>
+          <img
+            className="image-grid-col-2"
+            src="src/assets/images/model-potrait-1-min.jpg"
+            alt
+          />
+          <img src="src/assets/images/model-portrait-6-min.jpg" alt />
+          <img src="src/assets/images/model-portrait-4-min.jpg" alt />
+          <img src="src/assets/images/model-portrait-8-min.jpg" alt />
+          <img src="src/assets/images/aerial-shot-of-town.jpg" alt />
+          <img src="src/assets/images/man-on-boat.jpg" alt />
+          <img src="src/assets/images/golden-landscape.jpg" alt />
+          <img src="src/assets/images/pathway.jpg" alt />
+          <img src="src/assets/images/rice-field.jpg" alt />
+          <img src="src/assets/images/river.jpg" alt />
+          <img src="src/assets/images/horse-min.jpg" alt />
+          <img src="src/assets/images/landscape-with-man-min.jpg" alt />
+          <img src="src/assets/images/man-in-front-of-mountain-min.jpg" alt />
+          <img src="src/assets/images/man-looking-at-water-min.jpg" alt />
+          <img src="src/assets/images/photographer-min.jpg" alt />
+          <img src="src/assets/images/couple-facing-each-other-min.jpg" alt />
+          <img src="src/assets/images/couple-on-grass-min.jpg" alt />
+          <img src="src/assets/images/couple-standing-min.jpg" alt />
+          <img src="src/assets/images/portrait-of-guy-min.jpg" alt />
+          <img src="src/assets/images/potrait-of-couple-min.jpg" alt />
+        </div>
+      </section>
+      ;
+    </div>
+  );
 };
 
 export default Gallery;
