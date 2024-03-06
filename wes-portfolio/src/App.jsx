0a60efed2fbfa63dfda754aa228e5a9f234ae229
Maneesha <maneesha14w@gmail.com>
2024-03-06 12:08:36 +0530

    chore: header complete.

--- wes-portfolio/src/App.jsx
+++ wes-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
-import Header from "./components/Header";
-import Gallery from "./components/Gallery";
+import Header from "./components/Header/Header";
+import Gallery from "./components/Gallery/Gallery";
 import "./index.css";
 
 function App() {
---
+++ wes-portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-section">
+        <div className="image-container">
+          <img
+            src="src/assets/images/headshot-min.jpeg"
+            alt="Anh Tien taking a photo"
+          />
+        </div>
+        <div className="text-and-contact">
+          <div className="header-text">
+            <h1>Freelance Photographer and Visual Storyteller</h1>
+          </div>
+          <div className="subtext">
+            <h4>
+              Hi, I am Tien Anh Nguyen, a freelance photographer based in Ho Chi
+              Minh city, Vietnam. Whether I&apos;m documenting my personal life
+              or working for my clients, I&apos;m striving to create the best
+              photos possible. I believe that photos can make people&apos;s
+              lives better.
+            </h4>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
